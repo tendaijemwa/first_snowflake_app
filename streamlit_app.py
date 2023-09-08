ab4b33c672e3e19ec53f3a53aac2dc49469636b9
tendaijemwa <46998818+tendaijemwa@users.noreply.github.com>
2023-09-09 00:20:51 +1200

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
+  
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header(🥣'Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled Egg')
-streamlit.text('Maguru')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard Boiled Egg')
+streamlit.text(🥑🍞'Avocado Toast')
