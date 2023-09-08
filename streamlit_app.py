5d2244dd613ad4a0a00e5a52b740e29394e3547e
tendaijemwa <46998818+tendaijemwa@users.noreply.github.com>
2023-09-09 00:22:06 +1200

    Update streamlit_app.py
    Edited the file to correect omega

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
   
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header(🥣'Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
 streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
 streamlit.text(🐔'Hard Boiled Egg')
 streamlit.text(🥑🍞'Avocado Toast')
